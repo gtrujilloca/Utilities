262acef2ed0326e4be884161f71254ed11b9f3f4
Carlos García Trujillo <70419615+gtrujilloca@users.noreply.github.com>
2022-12-20 11:38:46 -0500

    Create useDebounce.ts

---
+++ React/useDebounce.ts
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+
+export const useDebounce = (value: string, delay: number) => {
+  const [debounceValue, setDebounceValue] = useState('');
+  useEffect(() => {
+    const timeoutId = setTimeout(() => setDebounceValue(value), delay);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [value]);
+
+  return debounceValue;
+};
